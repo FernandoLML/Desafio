a57ea0c15d6fcb0da0754885ea3788afbf039c7a
FernandoLML <fernandolucasmoraesdaluz@gmail.com>
2025-03-09 17:13:24 -0300

    Substring palindromica: Feito

--- substringPalindromica.js
+++ substringPalindromica.js
@@ -1,16 +1,38 @@
 const read = require ("readline-sync")
 
-function expandir
-
-function palindromo(str){
-
-    let normalizando = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-    let invertido = normalizando.split('').reverse().join('');
-
-    return normalizando === invertido;
-
+function expandirCentro(s, esquerda, direita) {
+    while (esquerda >= 0 && direita < s.length && s[esquerda] === s[direita]){
+        esquerda--;
+        direita++;
+    }
+    return s.slice(esquerda + 1, direita);
 }
 
+function maiorSubstringPalidromica(s){
+    if (!s || s.length === 0) return "";
+
+    let maiorPalindromo = "";
+
+    for (let i = 0; i < s.length; i++) {
+
+        let palindromo1 = expandirCentro(s, i, i);
+
+        let palindromo2 = expandirCentro(s, i, i + 1);
+
+        if (palindromo1.length > maiorPalindromo.length) {
+            maiorPalindromo = palindromo1;
+        }
+        if (palindromo2. length > maiorPalindromo.length) {
+            maiorPalindromo = palindromo2;
+        }
+    }
+
+    return maiorPalindromo;
+}
+
+
+
 let str = read.question("Digite uma string: ");
-console.log(`A string ${str} eh um palindromo? \n ${palindromo(str)}`);
+
+
+console.log(`A naior substring palindromica eh: "${maiorSubstringPalidromica(str)}"`);
